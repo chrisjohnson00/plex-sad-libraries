5147dd77413800d7cd91ca27979af21465bfc7a5
Chris Johnson <chrisjohnson00@hotmail.com>
2024-01-02 16:29:05 -0800

    feat: initial version

---
+++ redis/redis.py
@@ -0,0 +1,36 @@
+import json
+import os
+import redis
+import logging
+
+logger = logging.getLogger("sad.services.redis")
+logger.setLevel(os.environ.get("SAD_SERVICES_REDIS_LOG_LEVEL", "INFO"))
+logger.handlers = []
+console_handler = logging.StreamHandler()
+console_handler.setLevel(os.environ.get("SAD_SERVICES_REDIS_LOG_LEVEL", "INFO"))
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+def get_from_cache(*, key):
+    r = get_redis_connection()
+    value = r.get(key)
+    logger.debug(f"Fetched '{key}':'{value}' from cache")
+    return json.loads(value)
+
+
+def get_redis_connection():
+    redis_host = os.getenv("REDIS_HOST")
+    redis_port = os.getenv('REDIS_PORT', 6379)
+    r = redis.Redis(host=redis_host, port=redis_port, db=0)
+    return r
+
+
+def save_to_cache(*, key, data, ttl=0):
+    r = get_redis_connection()
+    logger.debug(f"Saving '{key}':'{data}' to cache")
+    if ttl > 0:
+        return r.set(key, json.dumps(data), ex=ttl)
+    else:
+        return r.set(key, json.dumps(data))
