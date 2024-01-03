3be93cee52383489e32f91c24a516f3a3f4de013
Chris Johnson <chrisjohnson00@hotmail.com>
2024-01-02 16:45:36 -0800

    feat: initial version

--- sad_libraries/tmdb/__init__.py
+++ sad_libraries/tmdb/__init__.py
@@ -1,2 +1,2 @@
-from sad_libraries.tmdb import search_movie_by_query_and_year
-from sad_libraries.tmdb import get_config
+from sad_libraries.tmdb.tmdb import search_movie_by_query_and_year
+from sad_libraries.tmdb.tmdb import get_config
