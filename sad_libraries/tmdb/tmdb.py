7ecc89f439d0b3da92e21b6b9f55a848a2e7ae7e
Chris Johnson <chrisjohnson00@hotmail.com>
2024-01-29 10:26:51 -0800

    feat: adding search for tv

--- sad_libraries/tmdb/tmdb.py
+++ sad_libraries/tmdb/tmdb.py
@@ -16,22 +16,11 @@ logger.addHandler(console_handler)
 
 
 def search_movie_by_query_and_year(*, query: str, year: int):
-    api_access_token = os.getenv("TMDB_API_ACCESS_TOKEN")
-    headers = {
-        "Authorization": f"Bearer {api_access_token}",
-    }
-    url = f"https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&query={quote(query)}&year={year}"  # noqa: E501
-    cached = sad_redis.get_from_cache(key=url)
-    if cached:
-        logger.debug(f"TMDB Cache hit for {query} ({year})")
-        return cached
-    logger.debug(f"TMDB Cache miss for {query} ({year}), requesting from TMDB")
-    r = requests.get(url, headers=headers)
-    r.raise_for_status()  # Raises an exception if not a 200 response code
-    response_json = json.loads(r.text)
-    logger.debug(f"Response url: '{url}' json: {response_json}")
-    sad_redis.save_to_cache(key=url, data=response_json, ttl=28800)
-    return response_json
+    return search(filters=[{"query": query, "year": year}], type="movie")
+
+
+def search_tv_by_title(*, title: str):
+    return search(filters=[{"query": title}], type="tv")
 
 
 def get_config():
@@ -52,3 +41,33 @@ def get_config():
     logger.debug(f"Response json: {response_json}")
     sad_redis.save_to_cache(key=url, data=response_json, ttl=604800)
     return response_json
+
+
+def search(*, filters: [dict], type: str):
+    api_access_token = os.getenv("TMDB_API_ACCESS_TOKEN")
+    headers = {
+        "Authorization": f"Bearer {api_access_token}",
+    }
+    query_string = encode_querystring(filters)
+    url = f"https://api.themoviedb.org/3/search/{type}?include_adult=false&language=en-US&{query_string}"
+    cached = sad_redis.get_from_cache(key=url)
+    if cached:
+        logger.debug(f"TMDB Cache hit for {url}), response: {cached}")
+        return cached
+    logger.debug(f"TMDB Cache miss for {url}), requesting from TMDB")
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()  # Raises an exception if not a 200 response code
+    response_json = json.loads(r.text)
+    sad_redis.save_to_cache(key=url, data=response_json, ttl=28800)
+    logger.debug(f"TMDB response for {url}, response {response_json}")
+    return response_json
+
+
+def encode_querystring(data):
+    encoded_params = []
+    for item in data:
+        for key, value in item.items():
+            encoded_key = quote(str(key))
+            encoded_value = quote(str(value))
+            encoded_params.append(f"{encoded_key}={encoded_value}")
+    return '&'.join(encoded_params)
